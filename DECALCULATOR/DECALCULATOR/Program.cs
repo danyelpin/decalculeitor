ac4baeed73ebce233c7a843055b28e54a995b7a6
Darlan-Benigno <darlan.benigno@gmail.com>
2025-08-18 19:32:29 -0300

    Tela de seleção

--- DECALCULATOR/DECALCULATOR/Program.cs
+++ DECALCULATOR/DECALCULATOR/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool loop = true;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("=-=-=-=-=-=-=-=-=-= DECALCULEITOR =-=-=-=-=-=-=-=-=-=");
+    Console.WriteLine("---Escolha uma das opções abaixo---");
+    Console.WriteLine("1 => Somar");
+    Console.WriteLine("2 => Subtrair");
+    Console.WriteLine("3 => Dividir");
+    Console.WriteLine("4 => Multiplicar");
+    Console.WriteLine("5 => Sair");
+    Console.WriteLine("Opção: ");
+
+    var opcao = Console.ReadLine();
+
+    switch (opcao)
+    {
+        case "1":
+            Somar();
+            break;
+        case "2":
+            Subtrair();
+            break;
+        case "3":
+            Dividir();
+            break;
+        case "4":
+            Multiplicar();
+        case "5":
+            loop = false;
+            Console.WriteLine("Saindo...");
+            Console.ReadKey();
+        default:
+            Console.WriteLine("Opção inválida.");
+            break;
+    }
+
+}
+while (loop);
