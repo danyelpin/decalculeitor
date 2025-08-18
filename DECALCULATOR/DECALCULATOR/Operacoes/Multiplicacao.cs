59a593da520bca392c51d6ee3286b0a458f8fc5b
AquilaMelo <aquilamelo2002@gmail.com>
2025-08-18 19:41:58 -0300

    Criei a pasta classes e a classe Uteis

--- DECALCULATOR/DECALCULATOR/Operacoes/Multiplicacao.cs
+++ DECALCULATOR/DECALCULATOR/Operacoes/Multiplicacao.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace DECALCULATOR.Operacoes
 {
-    internal class Multiplicacao
+    internal class Multiplicacao : IOperacoes
     {
+        public decimal Calcula(decimal numero1, decimal numero2)
+        {
+            return numero1 * numero2;
+        }
     }
 }
