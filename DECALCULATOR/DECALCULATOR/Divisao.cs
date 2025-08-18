2a255ddc0240932a93854dbd8797ceb26fb0c8b5
johnny.nascimento <nascimentoho1998@gmail.com>
2025-08-18 19:26:19 -0300

    - Adicionado classe de divisão.

---
+++ DECALCULATOR/DECALCULATOR/Divisao.cs
@@ -0,0 +1,10 @@
+﻿public class Divisao
+{
+    public decimal Calcula(decimal numero1, decimal numero2)
+    {
+        if (numero2 == 0)
+            return 0;
+
+        return numero1 / numero2;
+    }
+}
--- DECALCULATOR/DECALCULATOR/Program.cs
+++ DECALCULATOR/DECALCULATOR/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Divisao divisao = new Divisao();
+decimal resultadoDivisao = divisao.Calcula(10, 5);
+Console.WriteLine($"Resultado da divisão é: {resultadoDivisao}");
